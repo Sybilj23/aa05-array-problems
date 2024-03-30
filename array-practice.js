367042fc203b35fd0f4683ab3f14f938970470a1
sybil <jordan.sybil@hotmail.com>
2024-03-30 09:38:14 -0700

    test

--- array-practice.js
+++ array-practice.js
@@ -1,39 +1,40 @@
 const findMinimum = arr => {
 
-  // Your code here 
+  // Your code here
 
 };
 
 const runningSum = arr => {
 
-  // Your code here 
+  // Your code here
 };
 
 const evenNumOfChars = arr => {
 
-  // Your code here 
+  // Your code here
 };
 
 const smallerThanCurr = arr => {
 
-  // Your code here 
+  // Your code here
 
 };
 
 const twoSum = (arr, target) => {
 
-  // Your code here 
+  // Your code here
 };
 
 const secondLargest = arr => {
 
-  // Your code here 
+  // Your code here
 };
 
 const shuffle = (arr) => {
 
-  // Your code here 
+  // Your code here
 };
 
 
 module.exports = [findMinimum, runningSum, evenNumOfChars, smallerThanCurr, twoSum, secondLargest, shuffle];
+//test
