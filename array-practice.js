fc7d00dc6cfefef253f9e6932f69bc6ab8a86b83
sybil <jordan.sybil@hotmail.com>
2024-03-30 10:37:41 -0700

    almost done

--- array-practice.js
+++ array-practice.js
@@ -1,40 +1,69 @@
-const findMinimum = arr => {
-
-  // Your code here
-
+const findMinimum = (arr) => {
+  if (arr.length === 0) {
+    return undefined;
+  }
+  let min = Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return min;
 };
 
-const runningSum = arr => {
-
-  // Your code here
+const runningSum = (arr) => {
+  let sum = 0;
+  return arr.map((num) => (sum += num));
 };
 
-const evenNumOfChars = arr => {
-
-  // Your code here
+const evenNumOfChars = (arr) => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length % 2 === 0) {
+      count++;
+    }
+  }
+  return count;
 };
 
-const smallerThanCurr = arr => {
+const smallerThanCurr = (arr) => {
+  let array = [];
+  for (let i = 0; i < arr.length; i++) {
+    let count = 0;
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] < arr[i]) {
+        count++;
+      }
+    }
+    array.push(count);
+  }
 
-  // Your code here
-
+  return array;
 };
 
 const twoSum = (arr, target) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        return true;
+      }
+    }
+  }
 
-  // Your code here
+  return false;
 };
 
-const secondLargest = arr => {
+const secondLargest = (arr) => {};
 
-  // Your code here
-};
+const shuffle = (arr) => {};
 
-const shuffle = (arr) => {
-
-  // Your code here
-};
-
-
-module.exports = [findMinimum, runningSum, evenNumOfChars, smallerThanCurr, twoSum, secondLargest, shuffle];
-//test
+module.exports = [
+  findMinimum,
+  runningSum,
+  evenNumOfChars,
+  smallerThanCurr,
+  twoSum,
+  secondLargest,
+  shuffle,
+];
+//almost done
